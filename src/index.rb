fd69ee62aba50908a3aee851ec16753cbd99e48d
Louie Shprung <lshprung@scu.edu>
2022-11-21 17:50:01 -0800

    Rough type filtering (builtins, macros)

--- src/index.rb
+++ src/index.rb
@@ -11,15 +11,28 @@ INSERT_SQL = %Q[
 
 PATTERN = %r[<title>(.*)\(Autoconf\)(.*)</title>]
 
+BUILTIN_PATTERN = /The node you are looking for is at.*Limitations-of-.*\.html/
+MACRO_PATTERN = /The node you are looking for is at/
+
 def quote(s)
   s.gsub(/&amp;/, '&').gsub(/'/, "\\'").gsub(/&lt;/, '<')
 end
 
 ARGV.each do |arg|
   Pathname.glob(arg) do |path|
+    macro_match = path.each_line.lazy.map { |line| MACRO_PATTERN.match(line) }.find { |m| m }
+    builtin_match = path.each_line.lazy.map { |line| BUILTIN_PATTERN.match(line) }.find { |m| m }
+    if builtin_match
+      type = "Builtin"
+    elsif macro_match
+      type = "Macro"
+    else
+      type = "Guide"
+    end
+
     match = path.each_line.lazy.map { |line| PATTERN.match(line) }.find { |m| m }
     if match
-      printf INSERT_SQL, quote(match[1]), 'Guide', path.basename
+      printf INSERT_SQL, quote(match[1]), type, path.basename
     else
       $stderr.puts "%{path.basename}: no title found"
     end
